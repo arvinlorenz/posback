1bfd3df5b292c602f87ac28df3df899709285ab4
arvin <arvinlorenz@gmail.com>
2019-03-25 12:21:11 +0800

    wiht button fetch

--- src/app/order/processed/processed.component.ts
+++ src/app/order/processed/processed.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { OrderService } from '../order.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-processed',
   templateUrl: './processed.component.html',
   styleUrls: ['./processed.component.css']
 })
+
 export class ProcessedComponent implements OnInit {
   @Input() orders;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
   constructor() { }
 
   ngOnInit() {
+    this.dtTrigger.next();
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 100,
+      order: [[ 0, 'desc' ]],
+      "dom": '<"top"i>frt<"bottom"lp><"clear">',
+      processing: true,
+    };
+    
   }
 
 }
