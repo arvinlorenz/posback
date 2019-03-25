b58e7fa26f10deb5e283f5716e890714196a20da
arvin <arvinlorenz@gmail.com>
2019-03-25 14:36:34 +0800

    chinese gone

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://frozen-savannah-76475.herokuapp.com/api',
+  //apiUrl: 'https://frozen-savannah-76475.herokuapp.com/api',
+  apiUrl: 'https://linnworksapp-master.herokuapp.com/api',
   apiDB : 'apiToken'
 };
