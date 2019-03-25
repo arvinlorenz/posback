c552cf480950abbe3ec58e4ed24e886dd6fe95da
arvin <arvinlorenz@gmail.com>
2019-03-25 17:03:05 +0800

    changed uri env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://frozen-savannah-76475.herokuapp.com/api',
-  apiUrl: 'https://linnworksapp-grahamlisle .herokuapp.com/api',
+  apiUrl: 'https://linnworksapp-grahamlisle.herokuapp.com/api',
   apiDB : 'apiToken'
 };
