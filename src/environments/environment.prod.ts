a31631e5ce08b5073174c33bbe79dae82a55854a
arvin lorenz <arvinlorenz@gmail.com>
2020-09-30 13:26:26 +0800

    deleted table in process

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
   production: true,
-  //apiUrl: 'https://frozen-savannah-76475.herokuapp.com/api',
-  apiUrl: 'https://linnworksapp-master.herokuapp.com/api',
+  apiUrl: 'https://frozen-savannah-76475.herokuapp.com/api',
+  // apiUrl: 'https://linnworksapp-master.herokuapp.com/api',
    //apiUrl: 'https://linnworksapp-danielbenson.herokuapp.com/api',
-  
+
   apiDB : 'apiToken'
 };
